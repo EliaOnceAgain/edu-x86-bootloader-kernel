e5d347890e11902b40ee510a22004a72822640e9
elia.abu-nassar <eabunassar@gmail.com>
2023-01-04 16:45:25 +0200

    scheduler

---
+++ inc/scheduler.h
@@ -0,0 +1,39 @@
+#ifndef __KERNEL_SCHEDULER_H__
+#define __KERNEL_SCHEDULER_H__
+
+#include "process.h"
+
+int next_scheduled_pid, curr_scheduled_pid;
+process_t *next_process;
+
+/*
+ * DESCRIPTION:
+ * init globals to 0
+ */
+void init_scheduler();
+
+/*
+ * DESCRIPTION:
+ * called on system timer interrupts to choose the next process to run
+ * does context switch by copying the context of the current process
+ * from the registers to memory, and the context of the next process
+ * from memory to registers
+ */
+void scheduler(int, int, int, int, int, int, int, int, int);
+
+/*
+ * DESCRIPTION:
+ * execute next process
+ */
+void run_next_process();
+
+/*
+ * DESCRIPTION:
+ * get the next process that should run
+ *
+ * RETURN:
+ * process to run
+ */
+process_t *get_next_process();
+
+#endif /* __KERNEL_SCHEDULER_H__ */
